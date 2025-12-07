61bfa77ea3a36a5ff92eb10605470b669a7572eb
lathifni <lathifni.ni@gmail.com>
2025-12-07 20:30:35 +0700

    update method post

--- app/api/foods/route.js
+++ app/api/foods/route.js
@@ -61,3 +61,26 @@ export async function GET() {
     );
   }
 }
+
+export async function POST(req) {
+  try {
+    await connectDB();
+    const body = await req.json();
+
+    const food = await Food.create({
+      name: body.name,
+      category: body.category,
+      tags: body.tags || [],
+      weight: body.weight ?? 1,
+      isActive: body.isActive ?? true
+    });
+
+    return NextResponse.json(food, { status: 201 });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { message: "Failed to create food" },
+      { status: 500 }
+    );
+  }
+}
